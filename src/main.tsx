d3f4eec379195295043faa74ce2731207626017d
darunbjork <darunbjork@gmail.com>
2025-12-20 15:54:47 +0100

    feat: Wrap App with ThemeProvider in main.tsx
    - Imported  from .
    - Wrapped the  component with  in  to enable theme management throughout the application.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContextProvider.tsx'
+import { ThemeProvider } from './contexts/ThemeContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
